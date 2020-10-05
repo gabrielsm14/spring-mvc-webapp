ab0575231d33cb5641ce40dd9b8e8249a23fddb5
Gabriel mendonça <gabriel.mendonca14@outlook.com>
2020-10-05 16:12:48 -0300

    Spring MVC com thymeleaf e bootstrap

---
+++ src/main/java/br/com/mvc/mudi/MudiApplication.java
@@ -0,0 +1,14 @@
+package br.com.mvc.mudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class MudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/mvc/mudi/model/StatusPedido.java
@@ -0,0 +1,6 @@
+package br.com.mvc.mudi.model;
+
+public enum StatusPedido {
+
+	AGUARDANDO, APROVADO, ENTREGUE;
+}
