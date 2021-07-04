8a31cff9964dad7826d93f2f45ae6b441360d499
Gabriel <gabriel.mendonca14@outlook.com>
2021-07-03 22:06:57 -0300

    Implementando navbar, paginação e cache

--- src/main/java/br/com/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/mvc/mudi/MudiApplication.java
@@ -2,8 +2,10 @@ package br.com.mvc.mudi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Component;
 
+@EnableCaching
 @SpringBootApplication
 public class MudiApplication {
 
--- src/main/java/br/com/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/mvc/mudi/WebSecurityConfig.java
@@ -16,31 +16,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource dataSource;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and()
-				.formLogin(form -> form.loginPage("/login")
-				.defaultSuccessUrl("/usuario/pedido", true).permitAll())
-				.logout(logout -> logout.logoutUrl("/logout"))
-				.csrf().disable();
+		http.authorizeRequests().antMatchers("/home/**").permitAll().anyRequest().authenticated().and()
+				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/usuario/pedido", true).permitAll())
+				.logout(logout -> {
+					logout.logoutUrl("/logout").logoutSuccessUrl("/home");
+
+				});
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
+
 //		UserDetails user = User.builder()
 //				.username("maria")
 //				.password(encoder.encode("maria"))
 //				.roles("ADM")
 //				.build();
-		
-		auth.jdbcAuthentication()
-			.dataSource(dataSource)
-			.passwordEncoder(encoder);
+
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder);
 //			.withUser(user);
-			
+
 	}
-	
+
 }
